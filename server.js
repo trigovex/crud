c30d551d51598f3ec12dc41f20f0a20a4b39f826
SakethDwaraka24 <sakethdwarakagandham1919@gmail.com>
2025-08-12 23:12:40 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors()); // Allow frontend to connect
+app.use(express.json());
+
+app.use('/api/products', require('./routes/productRoutes'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
