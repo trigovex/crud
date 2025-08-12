e0feaca8b3ad4ab28a3faf84fde05b43e4fff69b
trigovex <trigovextechnologies@gmail.com>
2025-08-12 23:54:18 +0530

    first commit

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.use(express.json());
 app.use('/api/products', require('./routes/productRoutes'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server successfully running on port ${PORT}`));
